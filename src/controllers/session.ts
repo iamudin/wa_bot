51cabf73204493415ebbb2c58619dbebce5f6911
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2025-11-29 22:33:03 +0700

    uppdate

--- src/controllers/session.ts
+++ src/controllers/session.ts
@@ -5,8 +5,7 @@ import { z } from "zod";
 import { createKeyMiddleware } from "../middlewares/key.middleware";
 import { toDataURL } from "qrcode";
 import { HTTPException } from "hono/http-exception";
-import fs from "fs";
-import path from "path";
+
 export const createSessionController = () => {
   const app = new Hono();
 
@@ -27,19 +26,12 @@ export const createSessionController = () => {
     async (c) => {
       const payload = c.req.valid("json");
 
-     const sessionDir = path.join(__dirname, "../../wa_credentials", `${payload.session}_credentials`);
-
-    // Cek apakah folder session + minimal ada file JSON
-    const folderExists = fs.existsSync(sessionDir);
-    const hasJson =
-      folderExists &&
-      fs.readdirSync(sessionDir).some((f) => f.endsWith(".json"));
-
-    if (hasJson) {
-      throw new HTTPException(400, {
-        message: "Session already exist",
-      });
-    }
+      const isExist = whatsapp.getSession(payload.session);
+      if (isExist) {
+        throw new HTTPException(400, {
+          message: "Session already exist",
+        });
+      }
 
       const qr = await new Promise<string | null>(async (r) => {
         await whatsapp.startSession(payload.session, {
@@ -65,105 +57,52 @@ export const createSessionController = () => {
       });
     }
   );
-
   app.get(
-  "/start",
-  createKeyMiddleware(),
-  requestValidator("query", startSessionSchema),
-  async (c) => {
-    const payload = c.req.valid("query");
+    "/start",
+    createKeyMiddleware(),
+    requestValidator("query", startSessionSchema),
+    async (c) => {
+      const payload = c.req.valid("query");
 
-    // Path folder tempat session disimpan
-    const sessionDir = path.join(__dirname, "../../wa_credentials", `${payload.session}_credentials`);
+      const isExist = whatsapp.getSession(payload.session);
+      
+      if (isExist) {
+        throw new HTTPException(400, {
+          message: "Session already exist",
+        });
+      }
 
-    // Cek apakah folder session + minimal ada file JSON
-    const folderExists = fs.existsSync(sessionDir);
-    const hasJson =
-      folderExists &&
-      fs.readdirSync(sessionDir).some((f) => f.endsWith(".json"));
+      const qr = await new Promise<string | null>(async (r) => {
+        await whatsapp.startSession(payload.session, {
+          onConnected() {
+            r(null);
+          },
+          onQRUpdated(qr) {
+            r(qr);
+          },
+        });
+      });
 
-    if (hasJson) {
-      throw new HTTPException(400, {
-        message: "Session already exist",
+      if (qr) {
+        return c.render(`
+            <div id="qrcode"></div>
+
+            <script type="text/javascript">
+                let qr = '${await toDataURL(qr)}'
+                let image = new Image()
+                image.src = qr
+                document.body.appendChild(image)
+            </script>
+            `);
+      }
+
+      return c.json({
+        data: {
+          message: "Connected",
+        },
       });
     }
-
-    // Kalau belum ada file json, jalankan startSession
-    const qr = await new Promise<string | null>(async (r) => {
-      await whatsapp.startSession(payload.session, {
-        onConnected() {
-          r(null);
-        },
-        onQRUpdated(qr) {
-          r(qr);
-        },
-      });
-    });
-
-    if (qr) {
-      return c.render(`
-        <div id="qrcode"></div>
-        <script type="text/javascript">
-            let qr = '${await toDataURL(qr)}'
-            let image = new Image()
-            image.src = qr
-            document.body.appendChild(image)
-        </script>
-      `);
-    }
-
-    return c.json({
-      data: {
-        message: "Connected",
-      },
-    });
-  }
-);
-  // app.get(
-  //   "/start",
-  //   createKeyMiddleware(),
-  //   requestValidator("query", startSessionSchema),
-  //   async (c) => {
-  //     const payload = c.req.valid("query");
-
-  //     const isExist = whatsapp.getSession(payload.session);
-  //     if (isExist) {
-  //       throw new HTTPException(400, {
-  //         message: "Session already exist",
-  //       });
-  //     }
-
-  //     const qr = await new Promise<string | null>(async (r) => {
-  //       await whatsapp.startSession(payload.session, {
-  //         onConnected() {
-  //           r(null);
-  //         },
-  //         onQRUpdated(qr) {
-  //           r(qr);
-  //         },
-  //       });
-  //     });
-
-  //     if (qr) {
-  //       return c.render(`
-  //           <div id="qrcode"></div>
-
-  //           <script type="text/javascript">
-  //               let qr = '${await toDataURL(qr)}'
-  //               let image = new Image()
-  //               image.src = qr
-  //               document.body.appendChild(image)
-  //           </script>
-  //           `);
-  //     }
-
-  //     return c.json({
-  //       data: {
-  //         message: "Connected",
-  //       },
-  //     });
-  //   }
-  // );
+  );
 
   app.all("/logout", createKeyMiddleware(), async (c) => {
     await whatsapp.deleteSession(
